4b0c3157f8d169dcfc70e4b46d2a7f58b2100174
Rebecca <beckyufot@gmail.com>
2023-11-29 15:12:27 +0000

    first comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopBarComponent } from './header/top-bar/top-bar.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './container/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    HeaderComponent,
+    AppComponent,
+    TopHeaderComponent,
+    TopBarComponent,
+    MainMenuComponent,
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
